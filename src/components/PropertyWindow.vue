92374a50988537063ec521f41515dcc86a392a3e
Walter dos Santos Filho <waltersf@gmail.com>
2018-10-03 19:23:49 -0300

    Property window

---
+++ src/components/PropertyWindow.vue
@@ -0,0 +1,25 @@
+<template>
+    <VuePerfectScrollbar>
+        <div class="props">
+            <h4>{{$tc('titles.property', 2)}}</h4>
+        </div>
+    </VuePerfectScrollbar>
+</template>
+<script>
+    import VuePerfectScrollbar from 'vue-perfect-scrollbar'
+    export default {
+        name: 'PropertyWindow',
+        components: {
+            VuePerfectScrollbar
+        },
+    }
+</script>
+<style scoped>
+    .props {
+        background: #fff;
+        border: 1px solid #aaa;
+        padding: 10px 15px;
+        width: 350px;
+        height: calc(82vh + 2px)
+    }
+</style>
---
+++ src/components/SlideOutPanel.vue
@@ -0,0 +1,64 @@
+<template>
+    <transition name="slide">
+        <div class="slideout" v-if="opened">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'SlideOutPanel',
+        props: {
+            opened: false,
+        },
+        data: () => ({
+            openerText: 'Open',
+            menu: ['Home', 'Work', 'Contact'],
+            smallMenu: ['Tips', 'Resources', 'Shenanigans']
+        }),
+        methods: {
+            open() {
+                this.openerText = 'Close';
+                this.isOpen = true;
+            },
+            close() {
+                this.openerText = 'Open';
+                this.isOpen = false;
+            },
+            toggle() {
+                if (this.isOpen) {
+                    this.close();
+                } else {
+                    this.open();
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    .slideout {
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+
+    }
+
+    .slide-leave-active,
+    .slide-enter-active {
+        transition: .5s;
+    }
+
+    .slide-enter {
+        transform: translate(100%, 0);
+    }
+
+    .slide-leave-to {
+        transform: translate(100%, 0);
+    }
+
+    .opened {
+        display: block !important
+    }
+</style>
