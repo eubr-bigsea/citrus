1588290c9fc56e755b0bf2eb5f9a34900c83b268
Walter dos Santos Filho <waltersf@gmail.com>
2018-11-08 11:31:45 -0200

    Implementation [WIP]

--- src/views/WorkflowAdd.vue
+++ src/views/WorkflowAdd.vue
@@ -1,52 +1,51 @@
 <template>
     <div class="row">
-        <div class="col-md-12">
-            <h2>{{$t('actions.add', {type: $tc('titles.workflow').toLowerCase()})}}</h2>
-        </div>
-        <!--
-      <div class="col-md-6 mx-auto">
-        <form class="form-inline">
-            <label>{{$tc('common.name')}}: </label>
-            <input type="text" v-model="name" class="form-control" />
-            <label>{{$t('common.platform')}}: </label>
-            <select v-model="platformId" class="form-control">
-              <option v-for="platform in platforms" :key="platform.id" :value="platform.id">
-                {{platform.name}}
-              </option> 
-            </select>
-        </form>
-      </div>
-      -->
-        <div class="col-md-12">
-            <h3>By processing platform:</h3>
-        </div>
-        <div class="col-md-3 mb-2" v-for="platform in platforms" :key="platform.id">
-            <div class="card">
-                <div class="card-header text-white bg-secondary">
-                    {{platform.name}}
-                </div>
-                <div class="card-body">
-                    <p>{{platform.description}}</p>
-                    <button class="btn btn-success btn-sm float-right" @click="choose($event, platform.id)">
-                        {{$t('actions.choose')}}
-                    </button>
+        <div class="col-md-8 offset-md-2">
+            <div class="row border-bottom border-primary">
+                <div class="col-md-12">
+                    <h2>{{$t('actions.add', {type: $tc('titles.workflow').toLowerCase()})}}</h2>
                 </div>
             </div>
-        </div>
-        <div class="col-md-12">
-            <h3>By template:</h3>
-        </div>
-        <div class="col-md-2 mb-2" v-for="template in templates" :key="template.id">
-            <div class="card">
-                <div class="card-header text-white bg-secondary">
-                    {{template.name}}
-                </div>
-                <div class="card-body">
-                    <p>{{template.description}}</p>
-                    <button class="btn btn-success btn-sm float-right" @click="choose($event, platform.id)">
-                        {{$t('actions.choose')}}
-                    </button>
-                </div>
+            <div class="col-md-12">
+                <b-tabs>
+                    <b-tab title="By template" active>
+                        <div class="col-md-12 mt-2">
+                            <div class="d-flex flex-row justify-content-center">
+                                <div v-for="template, inx in templates" :key="inx" class="flex-grow-1 w-0">
+                                    <div class="card">
+                                        <div class="card-header text-white bg-secondary">
+                                            {{template.name}}
+                                        </div>
+                                        <div class="card-body">
+                                            <p>{{template.description}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </b-tab>
+                    <b-tab title="By processing platform">
+                        <div class="col-md-12 mt-2">
+                            <div class="d-flex flex-row justify-content-center">
+                                <div v-for="platform in platforms" :key="platform.id" class="flex-grow-1 w-0">
+                                    <div class="card h-100">
+                                        <div class="card-header text-white bg-secondary">
+                                            {{platform.name}}
+                                        </div>
+                                        <div class="card-body">
+                                            <p>{{platform.description}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </b-tab>
+                </b-tabs>
+            </div>
+            <div class="col-md-12 mt-3 border-top pt-1">
+                <button class="btn btn-success btn-sm" @click="choose($event, platform.id)">
+                    {{$t('actions.choose')}}
+                </button>
             </div>
         </div>
     </div>
@@ -61,11 +60,11 @@
             return {
                 platforms: [],
                 templates: [
-                    {id: 1, name: 'Classification', description: 'Train a classification model'},
-                    {id: 2, name: 'Regression', description: 'Train a regression model'},
-                    {id: 3, name: 'Clustering', description: 'Train a clustering model'},
-                    {id: 4, name: 'Frequent item set mining', description: 'Mining frequent item sets'},
-                    {id: 5, name: 'Deep Learning', description: 'Train a classification model using deep learning'},
+                    { id: 1, name: 'Classification', description: 'Train a classification model' },
+                    { id: 2, name: 'Regression', description: 'Train a regression model' },
+                    { id: 3, name: 'Clustering', description: 'Train a clustering model' },
+                    { id: 4, name: 'Frequent item set mining', description: 'Mining frequent item sets' },
+                    { id: 5, name: 'Deep Learning', description: 'Train a classification model using deep learning' },
                 ],
             }
         },
