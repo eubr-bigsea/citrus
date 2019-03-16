e30943ffcbceb2f121a78b07b8602005e4e7290c
Walter dos Santos Filho <waltersf@gmail.com>
2019-03-16 09:53:35 -0300

    Markdown widget

---
+++ src/components/widgets/Markdown.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-html="markdown"></div>
+</template>
+
+<script>
+    import snarkdown from 'snarkdown';
+    export default {
+        props: {
+            text: ""
+        },
+        computed: {
+            markdown() {
+                return snarkdown(this.text);
+            },
+        }
+    }
+</script>
