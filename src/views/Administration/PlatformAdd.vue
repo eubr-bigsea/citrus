b781d72ac5370f8f2dec0be3f2533b5e698c381e
Walter dos Santos Filho <waltersf@gmail.com>
2020-05-19 23:10:15 -0300

    Plaform listing and editing

---
+++ src/views/Administration/PlatformAdd.vue
@@ -0,0 +1,108 @@
+<template>
+  <main role="main">
+    <div class="row">
+      <div class="col">
+        <div>
+          <div class="d-flex justify-content-between align-items-center">
+            <h1>{{$t('actions.create', {type: $tc('titles.cluster', 1).toLowerCase()})}}</h1>
+          </div>
+          <hr>
+        </div>
+        <div class="row">
+          <div class="col-md-12 col-xg-12 mx-auto">
+            <div class="row">
+              <div class="col-md-6">
+                <label class="font-weight-bold">{{$tc('common.name')}}:</label>
+                <input v-model="cluster.name" type="text" class="form-control">
+              </div>
+              <div class="col-md-3">
+                <label class="font-weight-bold">{{$tc('common.type')}}:</label>
+                <select v-model="cluster.type" class="form-control">
+                  <option v-for="typ in types" :key="typ" :value="typ">{{typ}}</option>
+                </select>
+              </div>
+              <div class="col-md-3">
+                <label class="font-weight-bold">{{$t('cluster.address')}}:</label>
+                <input v-model="cluster.address" class="form-control">
+              </div>
+              <div class="col-md-3">
+                <label class="font-weight-bold">{{$tc('cluster.executors')}}:</label>
+                <input v-model="cluster.executors" class="form-control">
+              </div>
+              <div class="col-md-3">
+                <label class="font-weight-bold">{{$tc('cluster.executor_cores')}}:</label>
+                <input v-model="cluster.executor_cores" class="form-control">
+              </div>
+              <div class="col-md-3">
+                <label class="font-weight-bold">{{$tc('cluster.executor_memory')}}:</label>
+                <input v-model="cluster.executor_memory" class="form-control">
+              </div>
+              <div class="col-md-6 mt-3 mb-3 mt-3">
+                <b-form-checkbox v-model="cluster.enabled">{{ $t('common.enabled') }}</b-form-checkbox>
+              </div>
+              <div class="col-md-6">
+                <label>{{$tc('common.description')}}:</label>
+                <textarea v-model="cluster.description" class="form-control"></textarea>
+              </div>
+              <div class="col-md-6">
+                <label>{{$tc('common.general_parameters')}}:</label>
+                <textarea v-model="cluster.general_parameters" class="form-control"></textarea>
+              </div>
+            </div>
+            <div class="col-md-12 mb-4 border-top pt-2">
+              <button class="btn btn-primary mr-1 btn-spinner" @click.stop="save">
+                <font-awesome-icon icon="spinner" pulse class="icon"/>
+                <span class="fa fa-save"></span>
+                {{$tc('actions.save')}}
+              </button>
+              <router-link
+                :to="{name: 'clusters'}"
+                class="btn btn-secondary mr-1"
+              >{{$tc('actions.cancel')}}</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+import Notifier from '../../mixins/Notifier';
+
+let standUrl = process.env.VUE_APP_STAND_URL;
+
+export default {
+  name: 'ClusterAdd',
+  mixins: [Notifier],
+  data() {
+    return {
+      cluster: {},
+      types: ['SPARK_LOCAL', 'MESOS', 'YARN'].sort()
+    };
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    save(event) {
+      const data = this.cluster;
+
+      let url = `${standUrl}/clusters`;
+
+      axios
+        .post(url, data)
+        .then(resp => {
+          this.$router.push({
+            name: 'clusters'
+          });
+        })
+        .catch(
+          function(e) {
+            this.error(e);
+          }.bind(this)
+        );
+    }
+  }
+};
+</script>
---
+++ src/views/Administration/PlatformList.vue
@@ -0,0 +1,156 @@
+<template>
+        <div class="row">
+            <div class="col">
+                <div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h1>{{$tc('titles.platform', 2)}}</h1>
+                        <router-link :to="{name: 'addPlatform'}" class="btn btn-sm btn-outline-primary">
+                            {{$t('actions.addItem')}}</router-link>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <v-server-table ref="platformList" :columns="columns" :options="options"
+                                        name="platformList">
+                                        <template slot="id" slot-scope="props">
+                                            <router-link :to="{name: 'editPlatform', params: {id: props.row.id}}">
+                                                {{props.row.id}}</router-link>
+                                        </template>
+                                        <template slot="name" slot-scope="props">
+                                            <router-link :to="{name: 'editPlatform', params: {id: props.row.id}}">
+                                                {{props.row.name}}</router-link>
+                                        </template>
+                                        <template slot="enabled" slot-scope="props">
+                                            {{$tc(props.row.enabled ? 'common.yes': 'common.no')}}
+                                        </template>
+                                        <template slot="plugin" slot-scope="props">
+                                            {{$tc(props.row.plugin ? 'common.yes': 'common.no')}}
+                                        </template>
+                                        <template slot="actions" slot-scope="props">
+                                            <button class="btn btn-sm btn-light" @click="remove(props.row.id)">
+                                                <font-awesome-icon icon="trash"></font-awesome-icon>
+                                            </button>
+                                        </template>
+                                        <template slot="version" slot-scope="props">{{props.row.version}}</template>
+                                    </v-server-table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+</template>
+
+<script>
+    import axios from 'axios';
+    import { Event } from 'vue-tables-2';
+    import Notifier from '../../mixins/Notifier';
+
+    const tahitiUrl = process.env.VUE_APP_TAHITI_URL;
+
+    export default {
+        mixins: [Notifier],
+        data() {
+            return {
+                platform: '',
+                platforms: [],
+                columns: ['id', 'slug', 'name', 'description', 'enabled', 'plugin', 'version'],
+                options: {
+                    debounce: 800,
+                    skin: 'table-sm table table-hover',
+                    columnClasses: { actions: 'th-10' },
+                    headings: {
+                        id: 'ID',
+                        name: this.$tc('common.name'),
+                        description: this.$tc('common.description'),
+                        plugin: this.$tc('common.plugin'),
+                        slug: this.$tc('common.slug'),
+                        version: this.$tc('common.version'),
+                        enabled: this.$tc('common.enabled'),
+                        actions: this.$tc('common.action', 2)
+                    },
+                    sortable: ['name', 'id', 'slug',],
+                    //filterable: ['name', 'id', 'description'],
+                    filterable: false,
+                    sortIcon: {
+                        base: 'fa fas',
+                        is: 'fa-sort ml-10',
+                        up: 'fa-sort-amount-up',
+                        down: 'fa-sort-amount-down'
+                    },
+                    preserveState: true,
+                    saveState: true,
+                    customFilters: ['platform'],
+                    filterByColumn: false,
+                    requestFunction: function (data) {
+                        data.sort = data.orderBy;
+                        data.asc = data.ascending === 1 ? 'true' : 'false';
+                        data.size = data.limit;
+                        data.name = data.query;
+
+                        data.fields = 'id,name,type,address,enabled,plugin,version';
+
+                        let url = `${tahitiUrl}/platforms`;
+                        let headers = {};
+                        this.$Progress.start();
+                        return axios
+                            .get(url, { params: data })
+                            .then(resp => {
+                                this.$Progress.finish();
+                                return {
+                                    data: resp.data,
+                                };
+                            })
+                            .catch(
+                                function (e) {
+                                    this.$Progress.finish();
+                                    this.error(e);
+                                }.bind(this)
+                            );
+                    },
+                    texts: {
+                        filter: this.$tc('common.filter'),
+                        count: this.$t('common.pagerShowing'),
+                        limit: this.$t('common.limit'),
+                        noResults: this.$t('common.noData'),
+                        loading: this.$t('common.loading'),
+                        filterPlaceholder: this.$t('common.filterPlaceholder')
+                    }
+                }
+            };
+        },
+        methods: {
+            clearFilters() {
+                this.$refs.platformList.setFilter('');
+                this.$refs.platformList.customQueries = {};
+                this.platform = '';
+            },
+            disable(platformId) {
+                const self = this;
+                this.confirm(
+                    this.$t('actions.delete'),
+                    this.$t('messages.doYouWantToDelete'),
+                    () => {
+                        const url = `${tahitiUrl}/platforms/${platformId}`;
+
+                        axios
+                            .delete(url, {})
+                            .then(resp => {
+                                self.success(
+                                    self.$t('messages.successDeletion', {
+                                        what: this.$tc('titles.platform', 1)
+                                    })
+                                );
+                                self.$refs.platformList.refresh();
+                            })
+                            .catch(e => self.error(e));
+                    }
+                );
+            }
+        }
+    };
+</script>
