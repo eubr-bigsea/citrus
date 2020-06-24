e7b9386aa91c8330e229c09ad8b9c3779297cc0d
Walter dos Santos Filho <waltersf@gmail.com>
2020-06-24 18:36:34 -0300

    First version of views

---
+++ src/views/Track.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="mt-5">
+        <h1>Teste</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- src/views/TrackPanel.vue
+++ src/views/TrackPanel.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="container">
     <div class="row">
         <div class="col">
             <div>
@@ -8,51 +9,56 @@
                 <hr>
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <v-server-table :columns="columns" :options="options" ref="workflowList"
-                                    name="workflowList">
-                                    <template slot="id" slot-scope="props">
-                                        <router-link
-                                            :to="{name: 'editWorkflow', params: {id: props.row.id, platform: props.row.platform.id}}">
-                                            {{props.row.id}}</router-link>
-                                        </template>
-                                    <template slot="name" slot-scope="props">
-                                        <router-link
-                                            :to="{name: 'editWorkflow', params: {id: props.row.id, platform: props.row.platform.id}}">
-                                            {{props.row.name}}</router-link>
-                                            <small v-if="props.row.description" class="break-word"><br/>{{props.row.description}}</small>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><span class="fa fa-search"></span></span>
+                             </div>
+                             <input type="text" class="form-control" :placeholder="$t('track.whichTrack')">
+                         </div>
+                </div>
+                    <div class="col-md-12">
+                        <b-tabs content-class="mt-3" nav-class="custom-tab">
+                            <b-tab title="Recentes" active>
+                                <v-carousel :items="items" class="text-center">
+                                    <template v-slot:label="props">
+										<router-link :to="{name: 'trackParameter', params: {id: props.item.id}}">
+										{{props.item.name}}
+										</router-link>
+									</template>
+                                    <template v-slot:image="props">
+                                        <div class="text-justify m-2">
+										<router-link :to="{name: 'trackParameter', params: {id: props.item.id}}">
+                                            <img style="width:180px;height:auto" :src="props.item.image"/>
+										</router-link>
+                                        </div>
                                     </template>
-                                    <template slot="publishing_status" slot-scope="props">
-                                        <span v-if="props.row.publishing_status">
-                                           {{$t('track.' + props.row.publishing_status)}} 
-                                        </span>
-                                    </template>
-                                    <template slot="updated"
-                                        slot-scope="props">{{props.row.updated | formatJsonDate}}</template>
-                                    <div slot="afterFilter" class="ml-2 mt-4">
-                                        <button type="button"
-                                            class="btn btn-sm btn-light btn-outline-secondary ml-2"
-                                            @click="clearFilters">{{$tc('actions.clearFilters')}}</button>
-                                    </div>
-                                </v-server-table>
-                            </div>
-                        </div>
+                                </v-carousel>
+                            </b-tab>
+                            <b-tab title="Favoritos"><p>I'm the second tab</p></b-tab>
+                            <b-tab title="Novos"><p>I'm a disabled tab!</p></b-tab>
+                        </b-tabs>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios';
     import Notifier from '../mixins/Notifier';
+    import VCarousel from '../components/Carousel';
     let tahitiUrl = process.env.VUE_APP_TAHITI_URL;
     export default {
         mixins: [Notifier],
+        components: {
+            'v-carousel': VCarousel
+        },
         data() {
             return {
+                items: [],
                 columns: [
                     'id',
                     'name',
@@ -90,7 +96,7 @@
 
                         data.fields = 'id,name,platform,updated,user,version,description,publishing_status';
 
-                        let url = `${tahitiUrl}/workflows?enabled=1&track=1`;
+                        let url = `${tahitiUrl}/workflows?enabled=1&track=1&published=1`;
                         this.$Progress.start();
                         return axios
                             .get(url, {
@@ -122,9 +128,41 @@
             };
         },
         mounted() {
+            this.init();
         },
         /* Methods */
         methods: {
+            init(){
+                const self = this;
+                let data = {};
+                data.asc = 'true';
+                data.size = 100;
+                // data.name = ;
+                data.fields = 'id,name,platform,updated,user,version,description,publishing_status,image';
+
+                let url = `${tahitiUrl}/workflows?enabled=1&track=1&published=1`;
+                this.$Progress.start();
+                axios
+                    .get(url, {
+                        params: data
+                    })
+                    .then(resp => {
+                        this.$Progress.finish();
+                        self.items = resp.data.data;
+                        return {
+                            data: resp.data.data,
+                            count: resp.data.pagination ? resp.data.pagination.total : 
+                                resp.data.data.length,
+                        };
+                    })
+                    .catch(
+                        function (e) {
+                            this.$Progress.finish();
+                            this.error(e);
+                        }.bind(this)
+                    );
+ 
+            },
             clearFilters() {
                 this.$refs.workflowList.setFilter('');
                 this.$refs.workflowList.customQueries = {};
