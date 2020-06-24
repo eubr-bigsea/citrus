f6e1c2c730d12fe74625cff17a3436ba9439ce82
Walter dos Santos Filho <waltersf@gmail.com>
2020-06-24 18:40:29 -0300

    Use of cards to organize sections

--- src/views/modal/ModalWorkflowProperties.vue
+++ src/views/modal/ModalWorkflowProperties.vue
@@ -1,27 +1,39 @@
 <template>
     <b-modal size="lg" ref="modal" :title="$tc('titles.property', 2)">
         <b-form v-if="loaded">
-            <b-form-group :label="$tc('common.name', 1) + ':'">
-                <b-form-input type="text" v-model="workflow.name" required />
-            </b-form-group>
-            <b-form-group :label="$tc('common.description', 1) + ':'">
-                <b-form-textarea v-model="workflow.description" :rows="3" :max-rows="6" />
-            </b-form-group>
-            <SwitchComponent v-model="workflow.is_template" :checked="workflow.is_template" class="mt-3">
-                <strong>{{$t('workflow.useAsTemplate')}}</strong>
-            </SwitchComponent>
-            <small><em>{{$t('workflow.useAsTemplateExplanation')}}</em></small>
-            <div v-if="userPermissions.includes && userPermissions.includes('ADMINISTRATOR')">
-                <SwitchComponent v-model="workflow.is_system_template" :checked="workflow.is_system_template" class="mt-3">
-                    <strong>{{$t('workflow.useAsSystemTemplate')}}</strong>
-                </SwitchComponent>
-                <small><em>{{$t('workflow.useAsSystemTemplateExplanation')}}</em></small>
+            <div class="card">
+              <div class="card-body">
+                <b-form-group :label="$tc('common.name', 1) + ':'">
+                    <b-form-input type="text" v-model="workflow.name" required />
+                </b-form-group>
+                <b-form-group :label="$tc('common.description', 1) + ':'">
+                    <b-form-textarea v-model="workflow.description" :rows="3" :max-rows="6" />
+                </b-form-group>
+            </div>
+            </div>
+            <div class="card mt-2">
+              <div class="card-body">
+                <b-form-checkbox v-model="workflow.is_template">
+                    <small><strong>{{$t('workflow.useAsTemplate')}}</strong></small>
+                </b-form-checkbox>
+                <small><em>{{$t('workflow.useAsTemplateExplanation')}}</em></small>
+                <div v-if="userPermissions.includes && userPermissions.includes('ADMINISTRATOR')">
+                    <b-form-checkbox v-model="workflow.is_system_template">
+                        <small><strong>{{$t('workflow.useAsSystemTemplate')}}</strong></small>
+                    </b-form-checkbox>
+                    <small><em>{{$t('workflow.useAsSystemTemplateExplanation')}}</em></small>
+                </div>
+            </div>
             </div>
 
-            <SwitchComponent v-model="workflow.publishing_enabled" :checked="workflow.publishing_enabled" class="mt-3">
-                <strong>{{$t('workflow.publishingEnabled')}}</strong>
-            </SwitchComponent>
-            <small><em>{{$t('workflow.publishingEnabledExplanation')}}</em></small>
+            <div class="card mt-2">
+              <div class="card-body">
+              <b-form-checkbox v-model="workflow.publishing_enabled">
+                <small><strong>{{$t('workflow.publishingEnabled')}}</strong></small>
+              </b-form-checkbox>
+              <small><em>{{$t('workflow.publishingEnabledExplanation')}}</em></small>
+            </div>
+            </div>
         </b-form>
         <div slot="modal-footer">
             <b-btn variant="primary btn-sm" class="float-right mr-1" @click="okClicked" :disabled="(workflow.name === '')">
