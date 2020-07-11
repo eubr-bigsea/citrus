950d38debbcaf523f57e3775501cd287dd5a5134
Walter dos Santos Filho <waltersf@gmail.com>
2020-07-11 09:34:16 -0300

    Implementation and ajusts in visualization

---
+++ src/components/widgets/GridCoordinates.vue
@@ -0,0 +1,87 @@
+<template>
+    <div v-if="readOnly">
+        {{label}}
+    </div>
+    <div v-else class="mb-1">
+        <LabelComponent :field="field" :value="value"></LabelComponent>
+
+        <input disabled :value="label" class="form-control" />
+
+        <b-link @click.prevent="$refs.modal.show()" variant="sm">
+            {{$t('actions.changeOption')}}
+        </b-link>
+        
+        <b-modal size="lg" :title="`${field.label} - ${label || ''}`" ok-disabled
+            :cancel-title="$t('actions.cancel')" ref="modal" no-fade>
+            <small><em>{{field.help}}</em></small>
+
+            <div class="row border-top mt-2 pt-2">
+                <div class="offset-md-2 col-md-2">
+                    <label for="row">{{$tc('common.row', 1)}}</label>
+                    <input v-model="coordinates.row" type="number" min="0" max="100" name="x" class="form-control"/>
+                </div>
+                <div class="col-md-2">
+                    <label for="column">{{$tc('common.column', 1)}}</label>
+                    <input v-model="coordinates.column" type="number" min="0" max="12" name="y" class="form-control"/>
+                </div>
+                <div class="col-md-2">
+                    <label for="width">{{$tc('common.width', 1)}}</label>
+                    <input v-model="coordinates.width" type="number" min="1" max="12" name="width" class="form-control"/>
+                </div>
+                <div class="col-md-2">
+                    <label for="height">{{$tc('common.height', 1)}}</label>
+                    <input v-model="coordinates.height" type="number" min="1" max="10" name="height" class="form-control"/>
+                </div>
+            </div>
+            <div slot="modal-footer" class="w-100">
+                <b-btn @click="closeModal" variant="primary" class="ml-1 float-right">{{$t('common.ok')}}</b-btn>
+                <b-btn @click="removeValue" variant="outline-primary" class="float-right">{{$t('actions.removeValue')}}</b-btn>
+            </div>
+        </b-modal>
+    </div>
+</template>
+<script>
+    import LabelComponent from './Label.vue';
+    import Widget from '../../mixins/Widget.js';
+    export default {
+        mixins: [Widget],
+        components: { LabelComponent },
+        data() {
+            return {
+                coordinates: {
+                    row: 0,
+                    column: 0,
+                    height: 3,
+                    width: 12,
+                }
+            }
+        },
+        mounted() {
+            if (this.value){
+                this.coordinates = this.value;
+            }
+        },
+        computed: {
+            label(){
+                return `(${this.coordinates.row}, ${this.coordinates.column}, ${this.coordinates.width}, ${this.coordinates.height})`;
+            }
+        },
+        methods: {
+            removeValue() {
+                this.coordinates = {
+                    row: 0,
+                    column: 0,
+                    height: 0,
+                    width: 0,
+                };
+                this.$root.$emit(this.message, this.field, null, null);
+                this.closeModal();
+            },
+            closeModal() {
+                this.$root.$emit(this.message, this.field, this.coordinates, null);
+                this.$refs.modal.hide();
+            },
+        },
+    }
+
+</script>
---
+++ src/components/widgets/SampleLookup.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <LabelComponent :field="field" :value="value"></LabelComponent>
+        <div>
+            <v-select :options="suggestions" :multiple="multiple"
+                :value.sync="values" @input="updated" label="label" :taggable="false" :closeOnSelect="true"
+                :reduce="option => option.value">
+                <div slot="no-options"></div>
+            </v-select>
+        </div>
+    </div>
+</template>
+<script>
+    import vSelect from "vue-select";
+    import axios from 'axios';
+    import LabelComponent from './Label.vue'
+    import Widget from '../../mixins/Widget.js';
+
+    const limoneroUrl = process.env.VUE_APP_LIMONERO_URL;
+    /**
+     * Used to select value(s) from lookups filled with data 
+     * from Limonero. 
+     */
+    export default {
+        mixins: [Widget],
+        components: {
+            'v-select': vSelect,
+            LabelComponent
+        },
+        computed: {
+            multiple(){
+                return this.field.multiplicity === 'ONE_OR_MORE' || 
+                    this.field.multiplicity == 'ZERO_OR_MORE';
+            }
+        },
+        methods: {
+            updated(val) {
+                this.$root.$emit(this.message, this.field, val);
+            },
+            _loadLookup() {
+                const self = this;
+                const result = axios.get(`${limoneroUrl}/datasources/sample/${self.field.lookup}?limit=1000`, {})
+                    .then(result => {
+                        self.suggestions = result.data.data;
+                    });
+            }
+        },
+        mounted() {
+            this._loadLookup()
+        },
+        data() {
+            return {
+                values: [],
+                suggestions: [],
+            }
+        },
+        props: {
+            dataSourceId: null,
+            value: { default: [] },
+        },
+    }
+</script>
+<style>
+    .v-select ul.dropdown-menu {
+        font-size: 1em;
+    }
+</style>
