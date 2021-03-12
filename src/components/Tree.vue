4c3747d84ba1d54c011013717d7edd925ef053c7
Walter dos Santos Filho <waltersf@gmail.com>
2021-03-12 18:45:17 -0300

    New component

---
+++ src/components/Tree.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <TreeItem :item="tree" :opened="opened" :handle-click="selected" :bus="bus"/>
+    </div>
+</template>
+<script>
+    import Vue from "vue";
+    import TreeItem from './TreeItem';
+    export default {
+        components: { TreeItem },
+        props: {
+            tree: { type: Object, default: () => { } },
+            opened: false,
+        },
+        data(){
+            return {bus: new Vue()}
+        },
+        methods: {
+            selected(item) {
+                this.$emit('selected', item)
+            }
+        }
+
+    }
+</script>
