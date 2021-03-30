a8f57a7b81e5a4c351f0bbee78fe7776dcb4480f
Walter dos Santos Filho <waltersf@gmail.com>
2021-03-30 19:11:23 -0300

    Refactoring and code organization
    - Move some code to outside preview;
    - Implemented an overlay over selected column, with scroll support

--- src/components/data-explorer/PreviewMenu.vue
+++ src/components/data-explorer/PreviewMenu.vue
@@ -1,39 +1,47 @@
 <template>
-    <div class="border-top border-left border-right preview-menu">
-        <b-nav pills>
-            <b-nav-item-dropdown text="Editar" toggle-class="nav-link-custom" no-caret
-                :disabled="selected.key === undefined">
+    <div class="border preview-menu">
+        <b-nav>
+            <b-nav-item link-classes="bg-primary text-light">
+                <span class="fa fa-save"></span> {{$t('actions.save')}}
+            </b-nav-item>
+            <b-nav-item-dropdown text="Editar" toggle-class="nav-link-custom" :disabled="selected.label === undefined">
+                <template slot="button-content">
+                    <span class="fa fa-edit"></span> {{$t('actions.edit')}}
+                </template>
                 <b-dropdown-item @click="trigger('renameAttribute')">
-                    <span class="fa fa-edit text-secondary"></span> {{$t('actions.rename')}} coluna
+                    <span class="fa fa-edit text-secondary"></span> {{$t('actions.rename')}}
+                    {{$tc('common.attribute').toLowerCase()}}
                 </b-dropdown-item>
 
                 <b-dropdown-item class="ctx-divider"></b-dropdown-item>
 
                 <b-dropdown-item @click="trigger('deleteAttribute')">
-                    <span class="fa fa-times text-danger"></span> {{$t('actions.delete')}} coluna
+                    <span class="fa fa-times text-danger"></span> {{$t('actions.delete')}}
+                    {{$tc('common.attribute').toLowerCase()}}
                 </b-dropdown-item>
                 <b-dropdown-item @click="trigger('duplicateAttribute')" key="actionDuplicate">
                     <span class="fa fa-copy text-secondary"></span> {{$t('actions.duplicate')}}
-                    coluna
+                    {{$tc('common.attribute').toLowerCase()}}
                 </b-dropdown-item>
                 <b-dropdown-item @click="trigger('moveAttribute')" key="actionMove">
-                    <span class="fa fa-arrows-alt-h "></span> {{$t('actions.move')}} coluna
+                    <span class="fa fa-arrows-alt-h "></span> {{$t('actions.move')}}
+                    {{$tc('common.attribute').toLowerCase()}}
                 </b-dropdown-item>
                 <!--
-                <template v-if="selected.type === 'Array' ">
+                <template v-if="selected.field.type === 'Array' ">
                     <b-dropdown-item class="ctx-divider"></b-dropdown-item>
                     <b-dropdown-item key="parseAction">
                         <span class="fa fa-exclamation"></span> <b>Parse</b> with numerical range 
                     </b-dropdown-item>
                 </template>
-                <template v-if="selected.type === 'Boolean'">
+                <template v-if="selected.field.type === 'Boolean'">
                     <b-dropdown-item class="ctx-divider"></b-dropdown-item>
                     <b-dropdown-item key="negateAction">
                         <span class="fa fa-exclamation"></span> <b>Negate/invert</b> boolean values 
                     </b-dropdown-item>
                 </template>
 
-                <template v-if="selected.type === 'Date' || selected.type == 'Time' ">
+                <template v-if="selected.field.type === 'Date' || selected.field.type == 'Time' ">
                     <b-dropdown-item class="ctx-divider"></b-dropdown-item>
                     <b-dropdown-item>
                         <span class="fa fa-calendar"></span> <b>Parse</b> with numerical range 
@@ -41,17 +49,19 @@
                     <b-dropdown-item>
                         <b>Format</b> date 
                     </b-dropdown-item>
-                    <b-dropdown-item v-if="selected.type === 'Date'">
+                    <b-dropdown-item v-if="selected.field.type === 'Date'">
                         <b>Extract</b> date components 
                     </b-dropdown-item>
-                    <b-dropdown-item v-if="selected.type === 'Time'">
+                    <b-dropdown-item v-if="selected.field.type === 'Time'">
                         <b>Extract</b> time components 
                     </b-dropdown-item>
                 </template>
                 -->
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="Transformar" toggle-class="nav-link-custom" no-caret
-                :disabled="selected.key === undefined">
+            <b-nav-item-dropdown toggle-class="nav-link-custom" :disabled="selected.label === undefined">
+                <template slot="button-content">
+                    <span class="fa fa-magic"></span> {{$t('actions.transform')}}
+                </template>
                 <b-dropdown-item @click="trigger('changeAttributeType')">
                     {{$t('actions.changeDataType')}} para coluna
                 </b-dropdown-item>
@@ -62,21 +72,22 @@
                 <b-dropdown-item class="ctx-divider"></b-dropdown-item>
 
                 <b-dropdown-item @click="trigger('transform')" key="transformAction">
-                    <span class="fa fa-cogs text-primary"></span> <b>Transform</b> coluna with
+                    <span class="fa fa-magic text-primary"></span> <b>Transform</b> coluna with
                     custom function
                 </b-dropdown-item>
                 <b-dropdown-item class="ctx-divider"></b-dropdown-item>
-                <template v-if="selected.type === 'Integer' || selected.type == 'Decimal'">
+                <template v-if="selected.field.type === 'Integer' || selected.field.type == 'Decimal'">
                     <b-dropdown-item @click="trigger('redefineScale')">
                         <b>Redefine scale of</b> coluna
                     </b-dropdown-item>
-                    <b-dropdown-item v-if="selected.type == 'Decimal'" @click="trigger('transform', 'round', selected.label, 0)">
+                    <b-dropdown-item v-if="selected.field.type == 'Decimal'"
+                        @click="trigger('transform', 'round', selected.label, 0)">
                         <b>Round</b> coluna to integer
                     </b-dropdown-item>
                 </template>
 
 
-                <template v-if="selected.type === 'Integer' || selected.type == 'Decimal' ">
+                <template v-if="selected.field.type === 'Integer' || selected.field.type == 'Decimal' ">
                     <b-dropdown-item>Discretize (bin) (fixed, with/without start; custom)
                     </b-dropdown-item>
                     <b-dropdown-item>Normalize values using (FIXME: MinMax, etc)
@@ -86,19 +97,19 @@
                     <b-dropdown-item>Round numbers </b-dropdown-item>
                     <b-dropdown-item>Timestamp to date </b-dropdown-item>
                 </template>
-                <template v-if="selected.type === 'Text' ">
+                <template v-if="selected.field.type === 'Text' ">
                     <b-dropdown-item value="lower" @click="trigger('transform', 'lower', 'lower', selected.label)">
                         {{$t('dataExplorer.toLowerCase')}}
                     </b-dropdown-item>
                     <b-dropdown-item value="upper" @click="trigger('transform', 'upper', 'upper', selected.label)">
                         {{$t('dataExplorer.toUpperCase')}}
                     </b-dropdown-item>
-                    <b-dropdown-item @click="trigger('transform', 'title', selected.label)">
+                    <b-dropdown-item @click="trigger('transform', 'capitalizeFirst', 'title', selected.label)">
                         {{$t('dataExplorer.capitalizeFirst')}}
                     </b-dropdown-item>
                     <b-dropdown-item class="ctx-divider"></b-dropdown-item>
 
-                    <b-dropdown-item @click="trigger('transform', 'strip_accents', selected.label)">
+                    <b-dropdown-item @click="trigger('transform', 'removeAccents', 'strip_accents', selected.label)">
                         {{$t('dataExplorer.removeAccents')}}
                     </b-dropdown-item>
                     <b-dropdown-item>Concatenate with... </b-dropdown-item>
@@ -117,12 +128,12 @@
                     <b-dropdown-item>Extract with regex </b-dropdown-item>
 
                 </template>
-                <template v-if="selected.type === 'Array' ">
+                <template v-if="selected.field.type === 'Array' ">
                     <b-dropdown-item>Extract from array </b-dropdown-item>
                     <b-dropdown-item>Fold an array </b-dropdown-item>
                     <b-dropdown-item>Sort array </b-dropdown-item>
                 </template>
-                <template v-if="selected.type === 'Datetime' ">
+                <template v-if="selected.field.type === 'Datetime' ">
                     <b-dropdown-item>Update hour from </b-dropdown-item>
                     <b-dropdown-item>Truncate hour to 00:00 </b-dropdown-item>
                     <b-dropdown-item>Extract date elements </b-dropdown-item>
@@ -133,8 +144,11 @@
                 </template>
 
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="(De)codificar" toggle-class="nav-link-custom" no-caret
-                :disabled="selected.key === undefined">
+            <b-nav-item-dropdown text="(De)codificar" toggle-class="nav-link-custom"
+                :disabled="selected.label === undefined">
+                <template slot="button-content">
+                    <span class="fa fa-code"></span> {{$t('actions.encodeDecode')}}
+                </template>
                 <!--
                 <b-dropdown-item key="changeRepresentationAction">
                     <span class="fa fa-cogs text-primary"></span> <b>Change represention</b> with (One-Hot-Encoding)
@@ -150,8 +164,11 @@
                 <b-dropdown-item class="ctx-divider"></b-dropdown-item>
                 <b-dropdown-item>Pseudoanonymization </b-dropdown-item>
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="Sinalizar" toggle-class="nav-link-custom" no-caret
-                :disabled="selected.key === undefined">
+            <b-nav-item-dropdown text="Sinalizar" toggle-class="nav-link-custom"
+                :disabled="selected.label === undefined">
+                <template slot="button-content">
+                    <span class="fa fa-flag"></span> {{$t('actions.flag')}}
+                </template>
                 <b-dropdown-item>
                     <b>Flag</b> rows on range
                 </b-dropdown-item>
@@ -165,8 +182,11 @@
                     <b>Flag</b> rows with empty
                 </b-dropdown-item>
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="Corrigir" toggle-class="nav-link-custom" no-caret
-                :disabled="selected.key === undefined">
+            <b-nav-item-dropdown text="Corrigir" toggle-class="nav-link-custom"
+                :disabled="selected.label === undefined">
+                <template slot="button-content">
+                    <span class="fa fa-eraser"></span> {{$t('actions.fix')}}
+                </template>
                 <template>
                     <b-dropdown-item key="removeRowsEmptyCellsAction">
                         <span class="fa fa-fill-drip text-secondary"></span> Treat <b>empty value(s)</b>
@@ -197,21 +217,24 @@
                     -->
                     <!--
                     <b-dropdown-item>
-                        <b>Remove invalid rows</b> for meaning <em>{{selected.type}}</em>
+                        <b>Remove invalid rows</b> for meaning <em>{{selected.field.type}}</em>
                     </b-dropdown-item>
                     <b-dropdown-item>
-                        <b>Clear invalid cells</b> for meaning <em>{{selected.type}}</em>
+                        <b>Clear invalid cells</b> for meaning <em>{{selected.field.type}}</em>
                     </b-dropdown-item>
                     -->
                 </template>
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="Dados" toggle-class="nav-link-custom" no-caret>
+            <b-nav-item-dropdown toggle-class="nav-link-custom">
+                <template slot="button-content">
+                    <span class="fa fa-database"></span> {{$tc('common.data', 2)}}
+                </template>
 
-                <b-dropdown-item key="sortAsc" @click="trigger('sort', 'asc')" v-show="selected.key !== undefined">
+                <b-dropdown-item key="sortAsc" @click="trigger('sort', 'asc')" v-show="selected.label !== undefined">
                     <span class="fa fa-sort-alpha-up text-secondary"></span> <b>{{$t('actions.sort')}}</b> by
                     {{selected.label}} (ASC)
                 </b-dropdown-item>
-                <b-dropdown-item key="sortDesc" @click="trigger('sort', 'desc')" v-show="selected.key !== undefined">
+                <b-dropdown-item key="sortDesc" @click="trigger('sort', 'desc')" v-show="selected.label !== undefined">
                     <span class="fa fa-sort-alpha-down text-secondary"></span> <b>{{$t('actions.sort')}}</b> by
                     {{selected.label}} (DESC)
                 </b-dropdown-item>
@@ -237,6 +260,9 @@
                     <span class="fa fa-chart-bar text-info"></span> Analisar
                 </b-dropdown-item>
             </b-nav-item-dropdown>
+            <b-nav-item>
+                <span class="fa fa-cog"></span> Opções
+            </b-nav-item>
         </b-nav>
         <b-nav v-if="selected.label" class="mr-4 font-weight-bold">
             <b-nav-text><span class="fa fa-chevron-right"></span> {{selected.label}}</b-nav-text>
