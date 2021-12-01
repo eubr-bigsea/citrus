ecb764ed8ac35257dfba38e79cdf8d1b97a03640
Walter dos Santos Filho <waltersf@gmail.com>
2021-12-01 14:26:31 -0300

    Add missing file

---
+++ src/components/visualization/Html.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <a v-if="isFullHtml" :href="link" target="_blank">{{$t('actions.show')}} <span class="fa fa-external-link-alt"></span></a>
+        <div v-else :html="data"></div>
+    </div>
+</template>
+<script>
+    import VisualizationMixin from "./VisualizationMixin";
+    export default {
+        mixins: [VisualizationMixin],
+        data() {
+            return {
+                data: this.getData(),
+            };
+        },
+        computed: {
+            isFullHtml() {
+                return this.visualizationData.data
+                    .substring(0, 100).toLowerCase().indexOf('<html>') > -1;
+            },
+            link(){
+                let routeData = this.$router.resolve({name: 'displayHtml', params: {id: this.visualizationData.id}});
+                return routeData.href;
+            }
+        },
+        
+        mounted() {
+            /*if (this.isFullHtml) {
+                var doc = this.$refs.iframe.contentWindow.document;
+                doc.open();
+                doc.write(this.data);
+                doc.close();
+            }*/
+        },
+        methods: {
+            getData() {
+                return this.visualizationData.data;
+            },
+        }
+    }
+</script>
