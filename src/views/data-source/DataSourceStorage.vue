647a76e0e136f69fbad175be750fc2b68d52eb6e
Erick Sunclair <ericksunclair@hotmail.com>
2023-02-22 18:08:10 -0300

    finished the template of the data source storage issue

--- src/views/data-source/DataSourceStorage.vue
+++ src/views/data-source/DataSourceStorage.vue
@@ -3,34 +3,46 @@
         <div class="row">
             <div class="col">
                 <div>
-                    <div class="title">
-                        <h1>{{ $tc('titles.dataSource', 1) }}</h1>
+                    <div class="row">
+                        <div class="title col-md-2">
+                            <h1>{{ $tc('titles.dataSource', 1) }}</h1>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="row d-flex align-items-center text-secondary bg-light border rounded mx-1 p-2">
+                                {{ dataSource.name }}
+                            </div>
+                        </div>
                     </div>
+                    
                     <div class="row">
                         <div class="col-md-2">
                             <data-source-options selected="dataSourceStorage" />
                         </div>
                         <div class="col-md-10">
                             <!-- {{ dataSource.format === 'CSV' }} -->
-                            <div class="row border rounded mr-1 mb-1 py-3">
+                            <div class="row border rounded mx-1 py-3">
                                 <div class="col-md-2">
                                     Formato
                                 </div>
                                 <div class="col-md-10">
-                                    <b>{{ dataSource.format }}</b><br>
-                                    
-                                    <!-- roda essa parte se for CSV -->
-                                    <b>Importante:</b> Atualmente, a fonte de dados está armazenada usando o formato <b>CSV</b>.<br><br>
-                                    Esse formato não é otimizado para leitura, processamento e espaço em disco. Recomenda-se que seja usado um formato mais otimizado, especialmente quando a fonte de dados contiver milhares de registros ou centenas de atributos. O Lemonade suporta a conversão para o formato <b><u>Parquet</u></b>.<br><br>
-                                    Você poderá posteriormente exportar a fonte de dados novamente no formato CSV.<br><br>
+                                    <div v-if="dataSource.format === 'CSV'">
+                                        Atualmente, a fonte de dados está armazenada usando o formato <b>CSV</b>.<br><br>
+                                        <b>Importante:</b> Esse formato não é otimizado para leitura, processamento e espaço em disco. Recomenda-se que seja usado um formato mais otimizado, especialmente quando a fonte de dados contiver milhares de registros ou centenas de atributos. O Lemonade suporta a conversão para o formato <b><u>Parquet</u></b>.<br><br>
+                                        Você poderá posteriormente exportar a fonte de dados novamente no formato CSV.<br><br>
 
-                                    <input type="button" class="btn btn-primary" value="Converter para Parquet" v-on:click="convertToParquet">
-                                    
-                                    <br><br>    <!-- essa parte eu tiro depois -->
-                                    <!-- ou roda essa parte se for Parquet -->
-                                    Atualmente, a fonte de dados está armazenada usando o formato <b>Parquet</b>.<br><br>
-                                    Esse formato já é o formato recomendado para uso no Lemonade, por ser otimizado para leitura, processamento e espaço em disco. <br><br>
-                                    Você poderá posteriormente exportar a fonte de dados novamente no formato CSV.
+                                        <input type="button" class="btn btn-primary" value="Converter para Parquet" v-on:click="convertToParquet">
+                                    </div>
+
+                                    <div v-else-if="dataSource.format === 'PARQUET'">
+                                        Atualmente, a fonte de dados está armazenada usando o formato <b>Parquet</b>.<br><br>
+                                        Esse formato já é o formato recomendado para uso no Lemonade, por ser otimizado para leitura, processamento e espaço em disco. <br><br>
+                                        Você poderá posteriormente exportar a fonte de dados novamente no formato CSV.
+                                    </div>
+
+                                    <div v-else>
+                                        Atualmente, a fonte de dados está armazenada usando o formato <b>{{ dataSource.format }}</b>.
+                                    </div>
 
                                 </div>
                             </div>
@@ -63,9 +75,6 @@ export default {
             const resp = await axios.get(
                 `${limoneroUrl}/datasources/${this.$route.params.id}`);
             this.dataSource = resp.data;
-        },
-        convertToParquet() {
-            dataSource.format = 'Parquet';
         },
     },
 }
