e8a621d1e0328fdc979f8dfbd771ca5adfbe5d5b
pedrorfs <pedrorfs11@gmail.com>
2023-03-15 16:29:31 -0300

    Ajustes CSS do DataSourceAnalysis

--- src/views/data-source/DataSourceAnalysis.vue
+++ src/views/data-source/DataSourceAnalysis.vue
@@ -1,9 +1,61 @@
 <template>
     <main role="main">
-        <div class="row">
+        <div class="row header">
             <div class="title col-md-3">
                 <h1>{{ $tc('titles.dataSource', 1) }}</h1>
             </div>
+            <h2>{{ dataSource.name }}</h2>
+            <button v-b-modal.add-analysis type="button" class="btn btn-primary">
+                Adicionar...
+            </button>
+            <b-modal id="add-analysis" title="Adicionar Análise" class="row gx-5">
+                <label for="exampleFormControlSelect1">Atributos</label>
+                <select id="exampleFormControlSelect1" class="form-control">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+                <label for="exampleFormControlSelect1">Tipo</label>
+                <select id="exampleFormControlSelect1" class="form-control">
+                    <option @click="select(unidimensional)">Unidimensional</option>
+                    <option @click="select(bidimensional)">Bidimensional</option>
+                    <option @click="select(multidimensional)">Multidimensional</option>
+                    <option @click="select(testes)">Testes Estatísticos</option>
+                    <option @click="select(ajustar)">Ajustar (fit) a uma distribuição</option>
+                    <option @click="select(series)">Séries temporais</option>
+                </select>
+                <p>Análises:</p>
+                <div v-if="choice == unidimensional" class="flex column">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Histograma
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Quantile table
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Frequency table
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Summary stats
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Box Plot
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            Cumulative Distribution Function
+                        </label>
+                    </div>
+                </div>
+            </b-modal>
         </div>
 
         <div class="row">
@@ -12,69 +64,13 @@
             </div>
 
             <div class="col-md-8">
-                <div class="row">
-                    <div class="col d-flex flex-row justify-content-between">
-                        <div>{{ dataSource.name }}</div>
-                        <button v-b-modal.add-analysis type="button" class="btn btn-primary">
-                            Adicionar...
-                        </button>
-                        <b-modal id="add-analysis" title="Adicionar Análise" class="row gx-5">
-                            <label for="exampleFormControlSelect1">Atributos</label>
-                            <select id="exampleFormControlSelect1" class="form-control">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                            <label for="exampleFormControlSelect1">Tipo</label>
-                            <select id="exampleFormControlSelect1" class="form-control">
-                                <option @click="select(unidimensional)">Unidimensional</option>
-                                <option @click="select(bidimensional)">Bidimensional</option>
-                                <option @click="select(multidimensional)">Multidimensional</option>
-                                <option @click="select(testes)">Testes Estatísticos</option>
-                                <option @click="select(ajustar)">Ajustar (fit) a uma distribuição</option>
-                                <option @click="select(series)">Séries temporais</option>
-                            </select>
-                            <p>Análises:</p>
-                            <div v-if="choice == unidimensional" class="flex column">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Histograma
-                                    </label>
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Quantile table
-                                    </label>
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Frequency table
-                                    </label>
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Summary stats
-                                    </label>
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Box Plot
-                                    </label>
-                                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Cumulative Distribution Function
-                                    </label>
-                                </div>
-                            </div>
-                        </b-modal>
-                    </div>
-                </div>
-
-                <div class="row main_box">
-                    <div class="card">
-                        <h3>Atributo</h3>
-                        <div class="card-body">
-                            <div class="card-title">Histograma</div>
-
+                <div class="analysis">
+                    <h4>Unidimensional</h4>
+                    <div>
+                        <div class="analysis_card">
+                            <h6>Atributo</h6>
+                            <div class="analysis_card_title">Histograma</div>
+                            <div></div>
                         </div>
                     </div>
                 </div>
@@ -114,13 +110,29 @@ export default {
 </script>
 
 <style scoped>
-.main_box {
+
+.analisys {
+    display: flex;
+    flex-direction: column;
+}
+
+.analysis > div {
     height: 60vh;
-    border: 1px solid gray;
+    border: 1px solid #D9DADB;
     border-radius: 4px;
 }
 
-.card {
+.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10% 0 0;
+}
+
+.analysis_card {
+    display: flex;
+    flex-direction: column;
     width: 300px;
 }
 </style>
