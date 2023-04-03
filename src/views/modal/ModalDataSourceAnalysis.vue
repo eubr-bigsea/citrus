aa8d29768d0e770b094ab4abb5605d35715c5d19
pedrorfs <pedrorfs11@gmail.com>
2023-04-03 02:46:13 -0300

    Modify addCard function

--- src/views/modal/ModalDataSourceAnalysis.vue
+++ src/views/modal/ModalDataSourceAnalysis.vue
@@ -1,13 +1,13 @@
 <template>
     <b-modal id="modal" title="Adicionar Análise">
         <label for="exampleFormControlSelect1">Atributos</label>
-        <select id="exampleFormControlSelect1" class="form-control">
+        <select id="exampleFormControlSelect1" v-model="selectedAtributte" class="form-control">
             <option v-for="attr in atributtes" :key="attr.name" :value="attr.name">
                 {{attr.name}}
             </option>
         </select>
         <label for="exampleFormControlSelect1">Tipo</label>
-        <select id="analysis_type" class="form-control" v-model="selected">
+        <select id="analysis_type" v-model="selected" class="form-control">
             <option value="unidimensional">
                 Unidimensional
             </option>
@@ -27,8 +27,8 @@
                 Séries temporais
             </option>
         </select>
-        <b-form-group label="Análises:">
-            <b-form-checkbox-group stacked>
+        <b-form-group v-if="selected != null" label="Análises:">
+            <b-form-checkbox-group v-model="selectedGraphs" stacked>
                 <b-form-checkbox v-for="option in selectedAnalysisOptions" :key="option" :value="option">
                     {{option}}
                 </b-form-checkbox>
@@ -50,15 +50,17 @@
 export default {
     props: {
         atributtes: {
-            type: Object,
+            type: Array,
             default: null
         }
     },
     emits: ['cards'],
     data() {
         return {
-            selected: 'unidimensional',
-            analysis_options: {
+            selected: null,
+            selectedAtributte: null,
+            selectedGraphs: [],
+            analysisTypes: {
                 unidimensional: ['Histograma', 'Quantile Table', 'Frequency Table', 'Summary Stats', 'Box Plot', 'Cumulative distribution function'],
                 bidimensional: ['Mosaic Plot', 'Scatter Plot', 'Histogram', 'Frequency Table', 'Box Plot', 'Summary Test'],
                 multidimensional: ['PCA', 'Correlação', '...']
@@ -68,7 +70,7 @@ export default {
     computed: {
         selectedAnalysisOptions() {
             if (this.selected) {
-                return this.analysis_options[this.selected];
+                return this.analysisTypes[this.selected];
             }
             else {
                 return [];
@@ -77,7 +79,13 @@ export default {
     },
     methods: {
         addCard() {
-            this.$emit('cards', true);
+            let cardInfo = {
+                id: null,
+                analysisType: this.selected,
+                atributte: this.selectedAtributte,
+                graphs: this.selectedGraphs
+            };
+            this.$emit('cards', cardInfo);
         }
     }
 };
