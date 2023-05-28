a92742fe056957a8c6b00bbe7014593381ca3d04
pedrorfs <pedrorfs11@gmail.com>
2023-05-28 16:05:55 -0300

    Adjust layout and add methods

--- src/views/Administration/CodeAdd.vue
+++ src/views/Administration/CodeAdd.vue
@@ -1,17 +1,57 @@
 <template>
     <main role="main">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body d-flex justify-content-between flex-column ">
+                <div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h1>
+                            Adicionar Código
+                        </h1>
+                    </div>
+                    <hr>
+                </div>
                 <div class="row">
-                    <div class="col">
-                        <div>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h1>
-                                    Adicionar Código
-                                </h1>
-                            </div>
-                            <hr>
-                        </div>
+                    <div class="col-md-4">
+                        <label class="font-weight-bold">{{$tc('common.name')}}:</label>
+                        <br>
+                        <input v-model="codeName" type="text" class="form-control"
+                               required maxlength="100">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="enabled" class="font-weight-bold">Status:</label>
+                        <b-form-checkbox id="enabled" v-model="codeStatus">
+                            Habilitado
+                        </b-form-checkbox>
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="col-md-6">
+                        <label class="font-weight-bold">Descrição:</label>
+                        <br>
+                        <textarea id="" v-model="codeDescription" name="" class="form-control" 
+                                  rows="3" />
+                    </div>
+                </div>
+
+                <div class="row mt-4 mb-2">
+                    <div class="col-md-10">
+                        <input id="" type="file" name="" :change="addFile">
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-12 pt-2">
+                        <hr>
+                        <button class="btn btn-primary mr-1 btn-spinner" :on-click="addCode">
+                            <font-awesome-icon icon="spinner" pulse class="icon" />
+                            <font-awesome-icon icon="fa fa-save" />
+                            {{$tc('actions.save')}}
+                        </button>
+                        <router-link :to="{name: 'storages'}"
+                                     class="btn btn-secondary mr-1">
+                            {{$tc('actions.cancel')}}
+                        </router-link>
                     </div>
                 </div>
             </div>
@@ -21,8 +61,27 @@
 
 <script>
 
+export default {
+    data() {
+        return {
+            codeName: "",
+            codeStatus: null,
+            codeDescription: "",
+            codeFile: null
+        };
+    },
+    methods: {
+        addFile(e) {
+            this.codeFile = e.target.files;
+        },
+        addCode() {
+            // console.log(this.codeName, this.codeStatus, this.codeDescription, this.codeFile);
+            console.log("Oi");
+        }
+    }
+};
 </script>
 
-<!-- <style scoped>
+<style scoped>
 
-</style> -->
+</style>
