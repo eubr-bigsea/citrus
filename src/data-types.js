422f60b43898011004f711bd30d664797da49d48
Walter dos Santos Filho <waltersf@gmail.com>
2023-07-12 19:41:42 -0300

    Add missing

---
+++ src/data-types.js
@@ -0,0 +1,22 @@
+const NUMERIC = [
+    "DECIMAL",
+    "DOUBLE",
+    "FLOAT",
+    "INTEGER",
+    "LONG",
+];
+const OTHERS = [
+    "BINARY",
+    "CHARACTER",
+    "DATE",
+    "DATETIME",
+
+    "TEXT",
+    "TIME",
+    "TIMESTAMP",
+    "VECTOR",
+]
+function isNumeric(dataType) {
+    return NUMERIC.includes(dataType);
+}
+export {isNumeric};
