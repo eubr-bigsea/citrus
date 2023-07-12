89c47ad776775918bf29ee09c4dce6db2dbb1869
pedrorfs <pedrorfs11@gmail.com>
2023-07-12 16:18:45 -0300

    Add internacionalization

--- src/views/Administration/CodeLibrary.vue
+++ src/views/Administration/CodeLibrary.vue
@@ -1,50 +1,50 @@
 <template>
-  <main role="main">
-    <div class="row">
-      <div class="col">
-        <div>
-          <div class="d-flex justify-content-between align-items-center">
-            <h1>Listagem Arquivos</h1>
-            <router-link :to="{ name: 'codeAdd' }" class="btn btn-sm btn-success">
-              Adicionar Código Fonte
-            </router-link>
-          </div>
-          <hr>
-          <div class="row">
-            <div class="col-md-12">
-              <div class="card">
-                <div class="card-body">
-                  <v-client-table :data="tableData" :columns="columns" :options="options" name="codeLibrary">
-                    <template #status="props">
-                      <h1></h1>
-                    </template>
-                    <template #name="props">
-                      <h1></h1>
-                    </template>
-                    <template #id="props">
-                      <h1></h1>
-                    </template>
-                    <template #created="props">
-                      <h1></h1>
-                    </template>
-                    <template #actions="props">
-                      <button class="btn btn-sm btn-danger">
-                        <font-awesome-icon icon="trash" />
-                      </button>
-                      <router-link :to="{ name: 'codeEdit' }" class="btn btn-sm btn-primary ml-2">
-                        <font-awesome-icon icon="pen-to-square" size="lg"/>
-                      </router-link>
-                    </template>
+    <main role="main">
+        <div class="row">
+            <div class="col">
+                <div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h1>{{$t('titles.list', {elem: 'Arquivos'})}}</h1>
+                        <router-link :to="{ name: 'codeAdd' }" class="btn btn-sm btn-success">
+                            {{$t('actions.addSourceCode')}}
+                        </router-link>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <v-client-table :data="tableData" :columns="columns" :options="options" name="codeLibrary">
+                                        <template #status="props">
+                                            <h1></h1>
+                                        </template>
+                                        <template #name="props">
+                                            <h1></h1>
+                                        </template>
+                                        <template #id="props">
+                                            <h1></h1>
+                                        </template>
+                                        <template #created="props">
+                                            <h1></h1>
+                                        </template>
+                                        <template #actions="props">
+                                            <button class="btn btn-sm btn-danger">
+                                                <font-awesome-icon icon="trash" />
+                                            </button>
+                                            <router-link :to="{ name: 'codeEdit' }" class="btn btn-sm btn-primary ml-2">
+                                                <font-awesome-icon icon="pen-to-square" size="lg"/>
+                                            </router-link>
+                                        </template>
 
-                  </v-client-table>
+                                    </v-client-table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </main>
+    </main>
 </template>
 
 <script>
