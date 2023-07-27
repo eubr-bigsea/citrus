0866ef98cf75a5ab690edad626870ae8db38f34a
pedrorfs <pedrorfs11@gmail.com>
2023-07-27 11:35:11 -0300

    Add cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8081',
+    baseUrl: "http://localhost:8081",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
     },
   },
 });
