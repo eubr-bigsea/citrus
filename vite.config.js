2e76f19a7f2fdd4180921f0fefff638b369ba0de
Walter dos Santos Filho <waltersf@gmail.com>
2024-01-03 10:36:23 -0300

    Initial migration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 //import { createVuePlugin } from 'vite-plugin-vue2';
-import vue from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { visualizer } from "rollup-plugin-visualizer";
 import { manualChunksPlugin } from 'vite-plugin-webpackchunkname';
