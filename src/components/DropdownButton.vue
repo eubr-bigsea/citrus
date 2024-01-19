44aa8769ffc223db501a65c1154a0365db2fca5d
Walter dos Santos Filho <waltersf@gmail.com>
2024-01-19 13:35:08 -0300

    Add new component while vue-bootstrap-next does not work

---
+++ src/components/DropdownButton.vue
@@ -0,0 +1,63 @@
+<template>
+    <button class="btn dropdown-toggle" :class="buttonClass" @click.stop="toggleDropdown"
+        :aria-expanded="isDropdownOpen.toString()" ref="button" v-bind="$attrs">
+        <slot name="button-content">
+            {{ text }}
+        </slot>
+    </button>
+    <ul class="dropdown-menu" role="menu" ref="content" :class="{ show: isDropdownOpen }">
+        <slot name="content">
+        </slot>
+    </ul>
+</template>  
+<script setup>
+import { ref, computed, onBeforeUnmount } from 'vue';
+import { createPopper } from '@popperjs/core';
+
+let popperInstance = null;
+
+const button = ref();
+const props = defineProps({
+    variant: { type: String, required: false, default: 'secondary' },
+    text: { type: String, required: false, default: 'Dropdown' },
+    size: { type: String, required: true, default: '' },
+    noCaret: { type: Boolean, required: false, default: false },
+});
+const isDropdownOpen = ref(false);
+
+const buttonClass = computed(() => {
+    let result = (props.variant) ? `btn-${props.variant}` : '';
+    result += (props.size) ? ` btn-${props.size}` : '';
+    result += (props.noCaret) ? ' dropdown-toggle-no-caret' : '';
+    return result
+});
+
+const content = ref();
+const toggleDropdown = () => {
+
+    isDropdownOpen.value = !isDropdownOpen.value;
+    if (isDropdownOpen.value) {
+        document.body.addEventListener('click', closeDropdown);
+        popperInstance = createPopper(button.value, content.value, {
+            placement: 'bottom-start',
+        });
+    } else {
+        document.body.removeEventListener('click', closeDropdown);
+    }
+};
+const closeDropdown = () => {
+    isDropdownOpen.value = false;
+    document.body.removeEventListener('click', closeDropdown);
+    if (popperInstance) {
+        popperInstance.destroy();
+        popperInstance = null;
+    }
+};
+
+onBeforeUnmount(() => {
+    document.body.removeEventListener('click', closeDropdown);
+});
+</script>
+  
+<style scoped></style>
+  
