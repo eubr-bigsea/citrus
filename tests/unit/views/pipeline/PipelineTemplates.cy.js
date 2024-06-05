2265e269740b9237ce7711601111aa72e609387f
Vinícius Borges <viniborg25@gmail.com>
2024-06-05 08:37:50 -0300

    Add more tests for pipeline templates page

--- tests/unit/views/pipeline/PipelineTemplates.cy.js
+++ tests/unit/views/pipeline/PipelineTemplates.cy.js
@@ -3,12 +3,12 @@ import { createLocalVue } from '@vue/test-utils';
 import messages from '@/i18n/messages.js';
 import VueI18n from 'vue-i18n';
 import ModalCreateTemplate from "@/views/modal/ModalCreateTemplate.vue";
+import ModalEditTemplate from "@/views/modal/ModalEditTemplate.vue";
 
 let tahitiUrl = import.meta.env.VITE_TAHITI_URL;
 
 const localVue = createLocalVue();
 localVue.use(VueI18n);
-localVue.component('ModalCreateTemplate', ModalCreateTemplate);
 
 const i18n = new VueI18n({
     locale: 'pt', 
@@ -18,11 +18,19 @@ const i18n = new VueI18n({
 describe('<PipelineTemplates />', () => {
 
     beforeEach(() => {
-        cy.intercept("GET", `${tahitiUrl}/pipeline-templates`, { 
-            fixture: "pipelineTemplates.json"
-        }).as("getPipelineTemplates");
+        cy.intercept(
+            "GET", 
+            `${tahitiUrl}/pipeline-templates`, 
+            { 
+                fixture: "pipelineTemplates.json"
+            }
+        ).as("getPipelineTemplates");
 
         cy.mount(PipelineTemplates, {
+            components: {
+                ModalCreateTemplate,
+                ModalEditTemplate
+            },
             localVue,
             i18n,
         });
@@ -36,7 +44,7 @@ describe('<PipelineTemplates />', () => {
 
     });
 
-    it('Checks if the header is rendered correctly', () => {
+    it('checks if the header is rendered correctly', () => {
         
 
         cy.get('[data-test="header"]').should('be.visible');
@@ -45,10 +53,38 @@ describe('<PipelineTemplates />', () => {
         
     });
 
-    it('Checks if the table is rendered correctly', () => {
+    it('opens the add template modal when clicking the add button', () => {
+
+        cy.get('[data-test="addTemplateBtn"]').click();
+        cy.get('[data-test="addTemplateModal"]').should('be.visible');
+
+    });
+
+    it('checks if the table is rendered correctly', () => {
 
         cy.get('.templatePage-table').should('be.visible');
 
+    });
+
+    it('opens the edit template modal when clicking the edit button', () => {
+
+        cy.get('.templatePage-table tbody tr').first().find('button[title*="edit"]').click();
+        cy.get('.modal').should('be.visible');
+
+    });
+
+    it('can delete a template', () => {
+
+        cy.get('.templatePage-table tbody tr').first().find('button[title*="delete"]').click();
+        cy.on('window:confirm', () => true);
+        cy.get('.templatePage-table tbody tr').should('have.length.lessThan', 1);
+
+    });
+
+    it('sorts the templates table', () => {
+
+        cy.get('.templatePage-table th').contains('ID').click();
+        
     });
 
     it('Checks if the template creation process is working correctly', () => {
@@ -60,19 +96,12 @@ describe('<PipelineTemplates />', () => {
         const stepDescription = "Descrição da Etapa Teste.";
 
         cy.get('[data-test="addTemplateBtn"]').click();
-
         cy.get('[data-test="addTemplateModal"]').should('be.visible');
-
         cy.get('[data-test="input"]').type(templateTitle);
-
         cy.get('[data-test="textarea"]').type(templateDescription);
-
         cy.get('[data-test="addStepButton"]').click();
-
         cy.get('[data-test="stepInput"]').type(stepName);
-
         cy.get('[data-test="stepTextarea"]').type(stepDescription);
-
         cy.get('[data-test="closeButton"]').click();
 
     });
