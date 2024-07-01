6eb81188c6fb2a914364a361da0dae72d5edaa32
pedroMendesR <pedro.henrique.riopira@gmail.com>
2024-07-01 08:46:19 -0300

    feat: login command and cypress configurations

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,16 @@
-Cypress.Commands.add('login', (username, password) => {
-    cy.visit('/auth/login')
+Cypress.Commands.add('login', () => {
+    cy.session('Login', () => {
+        cy.visit('/auth/login')
 
-    cy.get('[data-test="email"]').type('waltersf@gmail.com').should('have.value', 'waltersf@gmail.com');
-    cy.get('[data-test="password"]').type('zooropa').should('have.value', 'zooropa');
-    cy.get('[data-test="login"]').click()
-    
-    // we should be redirected to /dashboard
-    cy.url().should('include', '/') // FIXME
-
-    // our auth cookie should be present
-    //cy.getCookie('your-session-cookie').should('exist')
-
-    // UI should reflect this user being logged in
-    //cy.get('h1').should('contain', username)
+        cy.get('[data-test="email"]').type('waltersf@gmail.com');
+        cy.get('[data-test="password"]').type('zooropa');
+        cy.get('[data-test="login"]').click();
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/home'); // FIXME
+        cy.getAllLocalStorage().then((result) => {
+            const {token, user} = result[Cypress.config().baseUrl]
+            expect(user).to.include('waltersf@gmail.com');
+            expect(token).to.exist;
+        });
+    });
 })
