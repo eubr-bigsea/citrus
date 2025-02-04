aebe7437b44ab190a47ef66d06744f0c04725240
Walter dos Santos Filho <waltersf@gmail.com>
2025-02-04 08:44:02 -0300

    WIP: define save options

--- src/views/data-explorer/model-builder/ModelBuilderSaveResults.vue
+++ src/views/data-explorer/model-builder/ModelBuilderSaveResults.vue
@@ -12,19 +12,19 @@
             </p>
         </div>
         <div class="col-6">
-            <b-form-checkbox v-model.number="formData.saveData" value="1" unchecked-value="0" @input="doEmit('saveData', $event)">
+            <b-form-checkbox v-model.number="formData.save_data" value="1" unchecked-value="0" @input="doEmit('save_data', $event)">
                 Aplicar e salvar os dados resultantes
             </b-form-checkbox>
-            <div v-if="formData.saveData == 1" class="row">
+            <div v-if="formData.save_data == 1" class="row">
                 <div class="col-12">
                     <label>Nome da nova fonte de dados:</label>
-                    <input v-model="formData.dataName" @input="doEmit('dataName', $event)" type="text"
+                    <input v-model="formData.data_name" @input="doEmit('data_name', $event)" type="text"
                         class="form-control form-control-sm" maxlength="100" />
                 </div>
                 <div class="col-12 mt-2">
                     <label>Armazenamento:</label>
-                    <select class="form-control form-control-sm" name="storage" v-model.number="formData.dataStorage"
-                        @change="doEmit('dataStorage', $event)">
+                    <select class="form-control form-control-sm" name="storage" v-model.number="formData.data_storage"
+                        @change="doEmit('data_storage', $event)">
                         <option></option>
                         <option v-for="st in storages" :key="st.id" :value="st.id">
                             {{ st.name }} ({{ st.type }})
@@ -33,13 +33,13 @@
                 </div>
                 <div class="col-12 mt-2">
                     <label>Caminho / pasta (opcional):</label>
-                    <input v-model="formData.dataPath" @input="doEmit('dataPath', $event)" type="text"
+                    <input v-model="formData.data_path" @input="doEmit('data_path', $event)" type="text"
                         class="form-control form-control-sm" maxlength="100">
                 </div>
                 <div class="col-12 mt-2">
                     <label>Opção de sobrescrita (se existir):</label>
-                    <select class="form-control form-control-sm" name="mode" v-model.number="formData.dataOverwrite"
-                        @change="doEmit('dataOverwrite', $event)">
+                    <select class="form-control form-control-sm" name="mode" v-model.number="formData.data_overwrite"
+                        @change="doEmit('data_overwrite', $event)">
                         <option value="error">Gerar erro (não salvar)</option>
                         <option value="ignore">Ignorar (não salvar)</option>
                         <option value="append">Acrescentar (append)</option>
@@ -49,20 +49,20 @@
             </div>
         </div>
         <div class="col-6">
-            <b-form-checkbox v-model.number="formData.saveModel" @input="doEmit('saveModel', $event)" value="1"
+            <b-form-checkbox v-model.number="formData.save_model" @input="doEmit('save_model', $event)" value="1"
                 unchecked-value="0">
                 Salvar o melhor modelo
             </b-form-checkbox>
-            <div v-if="formData.saveModel == 1" class="row">
+            <div v-if="formData.save_model == 1" class="row">
                 <div class="col-12">
                     <label>Nome do modelo:</label>
-                    <input v-model="formData.modelName" @input="doEmit('modelName', $event)" type="text"
+                    <input v-model="formData.model_name" @input="doEmit('model_name', $event)" type="text"
                         class="form-control form-control-sm" maxlength="100">
                 </div>
                 <div class="col-12 mt-2">
                     <label>Armazenamento:</label>
-                    <select class="form-control form-control-sm" name="storage" v-model.number="formData.modelStorage"
-                        @change="doEmit('modelStorage', $event)">
+                    <select class="form-control form-control-sm" name="storage" v-model.number="formData.model_storage"
+                        @change="doEmit('model_storage', $event)">
                         <option></option>
                         <option v-for="st in storages" :key="st.id" :value="st.id">
                             {{ st.name }} ({{ st.type }})
@@ -71,13 +71,13 @@
                 </div>
                 <div class="col-12 mt-2">
                     <label>Caminho / pasta (opcional):</label>
-                    <input v-model="formData.modelPath" @input="doEmit('modelPath', $event)" type="text"
+                    <input v-model="formData.model_path" @input="doEmit('model_path', $event)" type="text"
                         class="form-control form-control-sm" maxlength="100">
                 </div>
                 <div class="col-12 mt-2">
                     <label>Opção de sobrescrita (se existir):</label>
-                    <select class="form-control form-control-sm" name="mode" v-model.number="formData.modelOverwrite"
-                        @change="doEmit('modelOverwrite', $event)">
+                    <select class="form-control form-control-sm" name="mode" v-model.number="formData.model_overwrite"
+                        @change="doEmit('model_overwrite', $event)">
                         <option value="error">Gerar erro (não salvar)</option>
                         <option value="ignore">Ignorar (não salvar)</option>
                         <option value="append">Acrescentar (append)</option>
@@ -99,16 +99,16 @@ const props = defineProps({
     form: { type: Object, required: true }
 });
 const formData = ref({
-    saveData: props.form.saveData?.value,
-    saveModel: props.form.saveModel?.value,
-    dataName: props.form.dataName?.value,
-    modelName: props.form.modelName?.value,
-    dataStorage: props.form.dataStorage?.value,
-    modelStorage: props.form.modelStorage?.value,
-    dataPath: props.form.dataPath?.value,
-    modelPath: props.form.modelPath?.value,
-    dataOverwrite: props.form.dataOverwrite?.value || 'overwrite',
-    modelOverwrite: props.form.modelOverwrite?.value  || 'overwrite',
+    save_data: props.form.save_data?.value,
+    save_model: props.form.save_model?.value,
+    data_name: props.form.data_name?.value,
+    model_name: props.form.model_name?.value,
+    data_storage: props.form.data_storage?.value,
+    model_storage: props.form.model_storage?.value,
+    data_path: props.form.data_path?.value,
+    model_path: props.form.model_path?.value,
+    data_overwrite: props.form.data_overwrite?.value || 'overwrite',
+    model_overwrite: props.form.model_overwrite?.value  || 'overwrite',
 });
 
 const storages = ref([]);
