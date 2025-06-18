60fde75eba9347f59365a7a1f3eda5824568fdda
pedroMendesR <pedro.henrique.riopira@gmail.com>
2025-06-18 17:31:41 -0300

    peel url fetch fix

--- src/views/data-explorer/peel-platform/utils.js
+++ src/views/data-explorer/peel-platform/utils.js
@@ -3,16 +3,16 @@ import axios from "axios";
 export async function fetchPeelUrl() {
 
     const thornUrl = import.meta.env.VITE_THORN_URL;
-    const thornPeelVariable = 'PEEL_HOST'
+    const thornPeelVariable = 'SERVER_BASE_URL'
 
     try {
         const response = await axios.get(
             `${thornUrl}/configurations?query=&limit=50&ascending=1&page=1&byColumn=0&asc=true&size=50&name=&fields=name,value`
         );
         const peelUrl = response.data.data.find(obj => obj.name === thornPeelVariable)
-        console.log(peelUrl)
+        console.log(peelUrl.value)
         
-        return peelUrl;
+        return peelUrl.value;
     } catch (error) {
         console.error('Erro na requisição da url do Peel no thorn:', error);
       }
