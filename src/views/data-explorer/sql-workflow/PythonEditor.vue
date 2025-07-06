b41daf37fdd5f7d5e25926e5537e5d13261a1d1d
Walter dos Santos Filho <waltersf@gmail.com>
2025-07-06 12:15:11 -0300

    Correct the emit() call #471

--- src/views/data-explorer/sql-workflow/PythonEditor.vue
+++ src/views/data-explorer/sql-workflow/PythonEditor.vue
@@ -45,65 +45,7 @@ const sqlKeywords = ref(['ABS', 'ALL', 'AND', 'APPROXIMATE', 'AS', 'ASC',
 
 /* Methods */
 const focus = () => editor.value.focus();
-const sqlCompletion = (context) => {
-    const completions = [];
-    let before = context.matchBefore(/[\w\.]+/)
-    function apply(ctx, f) {
-        return (view, completion, from, to) => {
-            if (completion.type === 'method') {
-                return view.dispatch({
-                    changes: {
-                        from: from,
-                        to: to,
-                        insert: `${f}()`
-                    },
-                    selection: { anchor: from + f.length + 1 }
-                })
-            }
-            return autocompletion.apply(view, from, to, completion);
-        }
-    }
-    for (const f of props.functions) {
-        if (f === undefined) {
-            continue;
-        }
-        completions.push({
-            label: f,
-            detail: "função",
-            type: 'method',
-            apply: apply(context, f)
-        });
-    }
-    for (const t of props.tables) {
-        completions.push({
-            label: t.alias,
-            detail: "tabela",
-            type: 'class'
-        });
-        for (const a of t.attributes) {
-            completions.push({
-                label: `${t.alias}.${a.name}`,
-                detail: "coluna",
-                type: 'property'
-            })
-        }
-    }
-    for (const keyword of sqlKeywords.value) {
-        completions.push({
-            label: keyword,
-            detail: "palavra-chave",
-            type: 'keyword'
-        });
-    }
-    // If completion wasn't explicitly started and there
-    // is no word before the cursor, don't open completions.
-    if (!context.explicit && !before) return null
-    return {
-        from: before ? before.from : context.pos,
-        options: completions,
-        validFor: /^\w*$/
-    }
-}
+
 const indent = () => {
     //const formatted = format(props.query, props.format);
     //editor.value.dispatch({
@@ -142,7 +84,17 @@ onMounted(() => {
             python(),
             EditorView.lineWrapping,
             EditorView.updateListener.of(debounce((v) => {
-                emit('update', v.state.doc.text.join('\n'));
+                /*if( v.state.doc.children) {
+                    const newText = []
+                    v.state.doc.children.forEach((line, index) => {
+                        newText.push(line.text.join('\n'));
+                    });
+                    emit('update', newText.join('\n'));
+                } else {
+                    emit('update', v.state.doc.text.join('\n'));
+                }*/
+                const content = v.state.sliceDoc(0);
+                emit('update', content);
             }, 200))
         ],
         parent: container.value
