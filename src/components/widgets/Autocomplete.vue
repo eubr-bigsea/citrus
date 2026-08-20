3d276c149801ac8745812850158b40edf873ee26
Walter dos Santos Filho <walter.santos@almg.gov.br>
2026-08-20 15:29:57 -0300

    style: fix indentation in newly-restored Autocomplete.vue
    eslint --fix, scoped to this one file - it was dead/commented code
    until the previous commit, so it never went through lint before.

--- src/components/widgets/Autocomplete.vue
+++ src/components/widgets/Autocomplete.vue
@@ -29,93 +29,93 @@
      * Based on https://www.digitalocean.com/community/tutorials/vuejs-vue-autocomplete-component
     */
 export default {
-        name: "autocomplete",
-        props: {
-            items: {
-                type: Array,
-                required: false,
-                default: () => []
-            },
-            isAsync: {
-                type: Boolean,
-                required: false,
-                default: false
+    name: "Autocomplete",
+    props: {
+        items: {
+            type: Array,
+            required: false,
+            default: () => []
+        },
+        isAsync: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+            isOpen: false,
+            results: [],
+            search: "",
+            isLoading: false,
+            arrowCounter: 0
+        };
+    },
+    watch: {
+        items: function (val, oldValue) {
+            // actually compare them
+            if (val.length !== oldValue.length) {
+                this.results = val;
+                this.isLoading = false;
+            }
+        }
+    },
+    mounted() {
+        document.addEventListener("click", this.handleClickOutside);
+    },
+    unmounted() {
+        document.removeEventListener("click", this.handleClickOutside);
+    },
+
+    methods: {
+        onChange() {
+            // Let's warn the parent that a change was made
+            this.$emit("input", this.search);
+
+            // Is the data given by an outside ajax request?
+            if (this.isAsync) {
+                this.isLoading = true;
+            } else {
+                // Let's search our flat array
+                this.filterResults();
+                this.isOpen = true;
             }
         },
 
-        data() {
-            return {
-                isOpen: false,
-                results: [],
-                search: "",
-                isLoading: false,
-                arrowCounter: 0
-            };
+        filterResults() {
+            // first uncapitalize all the things
+            this.results = this.items.filter(item => {
+                return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+            });
         },
-
-        methods: {
-            onChange() {
-                // Let's warn the parent that a change was made
-                this.$emit("input", this.search);
-
-                // Is the data given by an outside ajax request?
-                if (this.isAsync) {
-                    this.isLoading = true;
-                } else {
-                    // Let's search our flat array
-                    this.filterResults();
-                    this.isOpen = true;
-                }
-            },
-
-            filterResults() {
-                // first uncapitalize all the things
-                this.results = this.items.filter(item => {
-                    return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
-                });
-            },
-            setResult(result) {
-                this.search = result;
-                this.isOpen = false;
-            },
-            onArrowDown() {
-                if (this.arrowCounter < this.results.length) {
-                    this.arrowCounter = this.arrowCounter + 1;
-                }
-            },
-            onArrowUp() {
-                if (this.arrowCounter > 0) {
-                    this.arrowCounter = this.arrowCounter - 1;
-                }
-            },
-            onEnter() {
-                this.search = this.results[this.arrowCounter];
+        setResult(result) {
+            this.search = result;
+            this.isOpen = false;
+        },
+        onArrowDown() {
+            if (this.arrowCounter < this.results.length) {
+                this.arrowCounter = this.arrowCounter + 1;
+            }
+        },
+        onArrowUp() {
+            if (this.arrowCounter > 0) {
+                this.arrowCounter = this.arrowCounter - 1;
+            }
+        },
+        onEnter() {
+            this.search = this.results[this.arrowCounter];
+            this.isOpen = false;
+            this.arrowCounter = -1;
+        },
+        handleClickOutside(evt) {
+            if (!this.$el.contains(evt.target)) {
                 this.isOpen = false;
                 this.arrowCounter = -1;
-            },
-            handleClickOutside(evt) {
-                if (!this.$el.contains(evt.target)) {
-                    this.isOpen = false;
-                    this.arrowCounter = -1;
-                }
             }
-        },
-        watch: {
-            items: function (val, oldValue) {
-                // actually compare them
-                if (val.length !== oldValue.length) {
-                    this.results = val;
-                    this.isLoading = false;
-                }
-            }
-        },
-        mounted() {
-            document.addEventListener("click", this.handleClickOutside);
-        },
-        unmounted() {
-            document.removeEventListener("click", this.handleClickOutside);
         }
-    };
+    }
+};
 </script>
 <style>
     .autocomplete {
