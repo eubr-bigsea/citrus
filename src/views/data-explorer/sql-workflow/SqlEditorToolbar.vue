1572da8ca23f67c1a3379c234a6f1535138441a3
Walter dos Santos Filho <waltersf@gmail.com>
2026-09-15 17:05:06 -0300

    Drop unneeded href=# from dropdown-items, plain click handlers

--- src/views/data-explorer/sql-workflow/SqlEditorToolbar.vue
+++ src/views/data-explorer/sql-workflow/SqlEditorToolbar.vue
@@ -35,16 +35,16 @@
                 <template #button-content>
                     <font-awesome-icon icon="fa fa-plus" />
                 </template>
-                <b-dropdown-item href="#" @click.prevent="emit('on-add', task.id, 'sql')">SQL</b-dropdown-item>
-                <b-dropdown-item href="#" @click.prevent="emit('on-add', task.id, 'python')">Python</b-dropdown-item>
-                <b-dropdown-item href="#" @click.prevent="emit('on-add', task.id, 'script')">Script externo</b-dropdown-item>
+                <b-dropdown-item @click="emit('on-add', task.id, 'sql')">SQL</b-dropdown-item>
+                <b-dropdown-item @click="emit('on-add', task.id, 'python')">Python</b-dropdown-item>
+                <b-dropdown-item @click="emit('on-add', task.id, 'script')">Script externo</b-dropdown-item>
             </b-dropdown>
             <b-dropdown variant="outline-success" right size="sm" no-caret  :title="$t('actions.execute')">
                 <template #button-content>
                     <font-awesome-icon icon="fa fa-play" />
                 </template>
-                <b-dropdown-item href="#" @click.prevent="emit('on-execute', task.id, true)">Até esta célula (usando dados do cache)</b-dropdown-item>
-                <b-dropdown-item href="#" @click.prevent="emit('on-execute', task.id, false)">Até esta célula (sem uso de dados do cache)</b-dropdown-item>
+                <b-dropdown-item @click="emit('on-execute', task.id, true)">Até esta célula (usando dados do cache)</b-dropdown-item>
+                <b-dropdown-item @click="emit('on-execute', task.id, false)">Até esta célula (sem uso de dados do cache)</b-dropdown-item>
             </b-dropdown>
             <button class="btn btn-sm btn-outline-secondary" title="Fechar" @click="emit('on-toggle', task.id)">
                 <font-awesome-icon :icon="opened ? 'fa fa-compress' : 'fa fa-expand'" />
