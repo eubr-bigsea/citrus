b79fe8964fa45cdd555660355feaf5b082f36d4a
Walter dos Santos Filho <waltersf@gmail.com>
2026-09-16 17:15:59 -0300

    Revert ModalSchedulePipeline.vue to separate month/year selects
    Undoes the input[type=month] simplification from 57de27b - month/year
    selects back, with the date-fns locale-based month name list and
    year range computed properties restored.

--- src/views/modal/ModalSchedulePipeline.vue
+++ src/views/modal/ModalSchedulePipeline.vue
@@ -8,24 +8,39 @@
         </p>
         <div class="row">
             <div class="col-6">
-                <input v-model="monthYear" type="month" class="form-control form-control-sm">
+                <select v-model.number="month" class="form-control form-control-sm">
+                    <option />
+                    <option v-for="m, i in getMonthNames" :key="m" :value="i + 1">
+                        {{m}}
+                    </option>
+                </select>
+            </div>
+            <div class="col-3">
+                <select v-model.number="year" class="form-control form-control-sm">
+                    <option />
+                    <option v-for="y in getYears" :key="y" :value="y">
+                        {{y}}
+                    </option>
+                </select>
             </div>
         </div>
         <div class="row mt-3">
             <div class="col-12">
                 <label>{{$t('pipeline.schedule.contextData')}}</label>
                 <div class="contextData">
-                    <div v-if="contextData" v-for="(pair, index) in contextData" :key="index"
-                        class="d-flex align-items-center mb-2">
-                        <input type="text" class="form-control form-control-sm me-2" v-model="pair.name"
-                            :placeholder="$t('pipeline.schedule.contextName')" maxlength="50" />
-                        <input type="text" class="form-control form-control-sm me-2" v-model="pair.value"
-                            :placeholder="$t('pipeline.schedule.contextValue')" maxlength="200" />
-                        <button type="button" class="btn btn-sm btn-danger ms-2" @click="removeContextData(index)">
-                            <font-awesome-icon icon="fa fa-trash" />
-                        </button>
-                    </div>
-                    <div v-if="contextData.length === 0" class="text-center">
+                    <template v-if="contextData.length">
+                        <div v-for="(pair, index) in contextData" :key="index"
+                            class="d-flex align-items-center mb-2">
+                            <input type="text" class="form-control form-control-sm me-2" v-model="pair.name"
+                                :placeholder="$t('pipeline.schedule.contextName')" maxlength="50" />
+                            <input type="text" class="form-control form-control-sm me-2" v-model="pair.value"
+                                :placeholder="$t('pipeline.schedule.contextValue')" maxlength="200" />
+                            <button type="button" class="btn btn-sm btn-danger ms-2" @click="removeContextData(index)">
+                                <font-awesome-icon icon="fa fa-trash" />
+                            </button>
+                        </div>
+                    </template>
+                    <div v-else class="text-center">
                         {{$t('pipeline.schedule.noContextData')}}
                     </div>
                 </div>
@@ -39,11 +54,37 @@
 
 <script setup>
 import { ref, computed } from 'vue';
+import { pt } from 'date-fns/locale';
+import { format } from 'date-fns';
 
-const monthYear = ref('');
+const month = ref(null);
+const year = ref(null);
 const name = ref(null);
 const id = ref(null);
 const scheduleModal = ref(null);
+
+const getMonthNames = computed(() => {
+    const monthNames = [];
+    for (let i = 0; i < 12; i++) {
+        const date = new Date(2021, i, 1); // Using any year, 2021 here
+        const monthName = format(date, 'MMMM', { locale: pt });
+        monthNames.push(monthName);
+    }
+    return monthNames;
+});
+
+const getYears = computed(() => {
+    const years = [];
+    let currentYear = new Date().getYear();
+    let maxYears = 5;
+    years.push(currentYear + 1901); // First year in JS is 1900! Add next year
+    do {
+        years.push(currentYear + 1900);
+        currentYear--;
+        maxYears--;
+    } while (maxYears > 0);
+    return years;
+});
 
 const show = (modalId, modalName) => {
 
@@ -52,15 +93,14 @@ const show = (modalId, modalName) => {
     scheduleModal.value.show();
 };
 const testFormOk = computed(() => {
-    return (!monthYear.value || contextData.value.some(data => !data.name || !data.value))
+    return (!month.value || !year.value || contextData.value.some(data => !data.name || !data.value))
 });
 const emit = defineEmits(['on-schedule-pipeline']);
 const confirm = () => {
-    const [year, month] = monthYear.value.split('-').map(Number);
     emit('on-schedule-pipeline', id.value,
         // In JS, months start from 0 !!!
-        new Date(year, month - 1, 1),
-        new Date(year, month, 0),
+        new Date(year.value, month.value - 1, 1),
+        new Date(year.value, month.value, 0),
         contextData.value
     );
 };
